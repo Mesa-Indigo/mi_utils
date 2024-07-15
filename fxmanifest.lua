83d79a6f75d66de2b27489dd1ebf7ce3a31c2d9b
Agimir <william@mesaindigostudios.com>
2024-07-15 10:35:52 -0500

    cleaned up things

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,11 +12,11 @@ repository 'https://github.com/Mesa-Indigo/mi_utils'
 description 'ox item handling script'
 
 --[[    dependancies    ]]--
-dependencies { '/server:7290', '/onesync', 'ox_lib', 'ox_target', 'ox_core' }
+dependencies { '/server:7290', '/onesync', 'ox_lib', 'ox_target' }
 
 --[[    manifest information    ]]--
-shared_scripts { '@ox_lib/init.lua', '@ox_core/lib/init.lua', 'shared/*.lua', 'data/*.lua' }
+shared_scripts { '@ox_lib/init.lua', 'shared/*.lua', 'data/*.lua' }
 
-client_scripts { '@ox_core/imports/client.lua', 'client/**/*.lua' }
+client_scripts { 'client/**/*.lua' }
 
-server_scripts { '@oxmysql/lib/MySQL.lua', '@ox_core/imports/server.lua', 'server/**/*.lua' }
+server_scripts { '@oxmysql/lib/MySQL.lua', 'server/**/*.lua' }
