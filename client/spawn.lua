6846662fd3a9a19cf79f00e362f08133b1cc445f
Agimir <william@mesaindigo.com>
2024-08-02 23:42:43 -0500

    working on spawn cutscene

---
+++ client/spawn.lua
@@ -0,0 +1,17 @@
+-- variables
+local cutscene = {
+    time = 31520,
+    music = 'AC_STOP',
+    peds = {
+        'a_f_m_bevhills_02', 'a_f_m_ktown_01', 'a_f_m_tourist_01', 'a_f_o_indian_01',
+        'a_f_o_soucent_02', 'a_f_y_indian_01', 'a_f_y_vinewood_01', 'a_f_y_gencaspat_01',
+        'mp_f_chbar_01', 'mp_f_execpa_01', 's_f_y_cop_01', 's_f_y_sheriff_01',
+        'a_m_m_bevhills_01', 'a_m_m_business_01', 'a_m_m_golfer_01', 'a_m_m_prolhost_01',
+        '', '', '', '',
+        '', '', '', '',
+    }
+}
+
+local selectped = function()
+
+end
